da336b9c3f0c22bb6e547c1549529f41745edb81
tk-yoshimura <yoshimura8192@gmail.com>
2024-02-17 19:12:55 +0900

    add: entropy

---
+++ HoltsmarkEvalExpected/EvalEntropy.cs
@@ -0,0 +1,39 @@
+﻿using HoltsmarkPadeApprox;
+using MultiPrecision;
+using MultiPrecisionIntegrate;
+
+namespace HoltsmarkEvalExpected {
+    internal class EvalEntropy {
+        static void Main() {
+            using StreamWriter sw = new("../../../../results/entropy_precision60.csv");
+
+            static MultiPrecision<Pow2.N16> info(MultiPrecision<Pow2.N16> x) {
+                MultiPrecision<Pow2.N16> px = PDFPadeN16.Value(x);
+
+                if (px == 0) {
+                    return 0;
+                }
+
+                return -px * MultiPrecision<Pow2.N16>.Log(px);
+            };
+
+            (MultiPrecision<Pow2.N16> value, MultiPrecision<Pow2.N16> error) =
+                GaussKronrodIntegral<Pow2.N16>.AdaptiveIntegrate(info, 0, MultiPrecision<Pow2.N16>.PositiveInfinity, 
+                1e-120, GaussKronrodOrder.G32K65, 128
+            );
+
+            value *= 2;
+            error *= 2;
+
+            Console.WriteLine($"{value}\n{error:e20}");
+
+            sw.WriteLine($"entropy,error");
+            sw.WriteLine($"{value},{error:e20}");
+
+            sw.Close();
+
+            Console.WriteLine("END");
+            Console.Read();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -42,11 +42,13 @@ Evaluating only one term of this equation yields the tail power of the distribut
 |----|----|----|
 |mean|0||
 |mode|0||
+|median||
 |variance|N/A|undefined|
 |0.75-quantile|0.9689331817135830052087863130496...||
 |0.9-quantile|2.061462638139193764464365388228...||
 |0.95-quantile|3.051940973238316697189330345356...|
 |0.99-quantile|7.736446206485418555263326182069...||
+|entropy|2.069448505134624400315580038454...||
 
 ## Numeric Table
 [PDF Precision 150](results/pdf_precision150.csv)  
